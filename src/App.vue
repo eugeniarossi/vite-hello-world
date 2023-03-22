26582ed95894b52c5bcef7b2812f033ab17e8711
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-22 17:45:21 +0100

    feat: jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script>
   // importo il componente AppTitle
   import AppTitle from './components/AppTitle.vue';
+  // importo il componente AppHero
+  import AppHero from './components/AppHero.vue';
   // rendo disponibili i componenti
   export default {
     components: {
-      AppTitle
+      AppTitle,
+      AppHero
     }
   }
 </script>
@@ -12,6 +15,7 @@
 <template>
   <!-- uso il componente AppTitle come un tag -->
   <AppTitle></AppTitle>
+  <AppHero></AppHero>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+    // esporto ciò che voglio rendere disponibile
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<template>
+  <div id="container">
+    <img src="../img/rickandmorty.jpg" alt="rick & morty">
+  </div>
+</template>
+
+<style scoped>
+    img {
+        max-width: 100%;
+        display: block;
+    }
+</style>
